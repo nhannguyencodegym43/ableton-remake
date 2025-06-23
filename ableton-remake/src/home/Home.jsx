7bed75ce83a0ab176bfe83e04d8a3016bc113444
trungnhan_nguyen <nhannguyen6533@gmail.com>
2025-06-23 10:39:20 +0700

    basic navbar

---
+++ ableton-remake/src/home/Home.jsx
@@ -0,0 +1,12 @@
+import NavBar from "./components/nav_bar/nav_bar.jsx";
+
+function Home() {
+
+    return (
+        <>
+            <NavBar />
+        </>
+    )
+}
+
+export default Home
---
+++ ableton-remake/src/home/components/nav_bar/nav_bar.jsx
@@ -0,0 +1,30 @@
+import "./style.css"
+function NavBar() {
+
+    return (
+        <>
+            <div className="navbar-container">
+                <div className="logo">
+                    <a href="#home"><img src="../../../../public/home/logo_ableton.PNG" alt="Ableton Remake Logo" /></a>
+                </div>
+                <div className="nav-links">
+                    <a href="#live">Live</a>
+                    <a href="#push">Push</a>
+                    <a href="#move">Move</a>
+                    <a href="#note">Note</a>
+                    <a href="#link">Link</a>
+                    <a href="#shop">Shop</a>
+                    <a href="#packs">Packs</a>
+                    <a href="#help">Help</a>
+                    <a href="#more">More +</a>
+                </div>
+                <div className="try-login">
+                    <a href="#try" className="try">Try Live 12 for free</a>
+                    <a href="#login">Log in or register</a>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default NavBar
